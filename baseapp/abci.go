337b6e7a2786ab9813932c915e682ac336c6c244
Tanapat Sainak <fallen_things@hotmail.com>
2024-05-27 06:53:55 +0700

    add log

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -121,6 +121,8 @@ func (app *BaseApp) InitChain(req *abci.RequestInitChain) (*abci.ResponseInitCha
 
 		for i := range res.Validators {
 			if !proto.Equal(&res.Validators[i], &req.Validators[i]) {
+				fmt.Errorf("res.Validator[%d]: %+v\n", i, res.Validators[i])
+				fmt.Errorf("req.Validator[%d]: %+v\n", i, req.Validators[i])
 				return nil, fmt.Errorf("genesisValidators[%d] != req.Validators[%d] ", i, i)
 			}
 		}
